7698ecda4a276ba187f4eb74c965898aba49f26c
Samaan Khella (EXT-Nokia) <samaan.khella.ext@nokia.com>
2025-09-22 10:40:56 +0300

    added annotation on top of main class to read properties file

--- backend/Pagination/src/main/java/com/example/Pagination/PaginationApplication.java
+++ backend/Pagination/src/main/java/com/example/Pagination/PaginationApplication.java
@@ -2,8 +2,10 @@ package com.example.Pagination;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:env.properties")
 public class PaginationApplication {
 
 	public static void main(String[] args) {
