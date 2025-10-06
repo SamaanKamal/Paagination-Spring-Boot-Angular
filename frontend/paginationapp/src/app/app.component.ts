71b0f3f224d63d3cc20674afbc3b84d3ca27f720
Samaan Khella (EXT-Nokia) <samaan.khella.ext@nokia.com>
2025-10-06 16:40:55 +0300

    loaded the users when the project starts

--- frontend/paginationapp/src/app/app.component.ts
+++ frontend/paginationapp/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { catchError, map, Observable, of, startWith } from 'rxjs';
+import { ApiResponse } from './interface/api-response';
+import { Page } from './interface/page';
+import { HttpErrorResponse } from '@angular/common/http';
+import { UserService } from './service/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'paginationapp';
+export class AppComponent implements OnInit {
+  title : string = "Pagination APP";
+  usersState$: Observable<{ appState: string, appData?: ApiResponse<Page>, error?: HttpErrorResponse }>;
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.usersState$ = this.userService.$user().pipe(
+      map((response: ApiResponse<Page>) => {
+        console.log(response);
+        return ({ appState: 'APP_LOADED', appData: response });
+      }
+      ),
+      startWith({ appState: 'APP_LOADING' }),
+      catchError((error: HttpErrorResponse) => {
+        return of({ appState: 'APP_ERROR', error });
+      })
+    );
+  }
 }
